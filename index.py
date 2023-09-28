6f7abf17b9fc53ac402c9ecc0e4663d0d23ef282
Vatsal Mishra <115968479+Vatsal2024@users.noreply.github.com>
2023-09-28 14:40:22 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,6 @@
 # Print a pyramid pattern
 rows = 5
-
-# Outer loop iterates over the rows
 for i in range(1, rows + 1):
-  # Inner loop iterates over the columns
-  for j in range(1, i + 1):
-    print("*", end='')
-  print()
+    for j in range(1, i + 1):
+        print("*", end="")
+    print()
